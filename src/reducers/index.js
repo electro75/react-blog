5af251a1b356797183ea8d72d7b26f6b0c7e4615
sahil n <sahil@mutterfly.in>
2018-12-21 16:13:00 +0530

    adds reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
 
 export default combineReducers({
-    dummyReducer : () => {
-        return 'replace me'
-    }
+    posts: postsReducer
 })
