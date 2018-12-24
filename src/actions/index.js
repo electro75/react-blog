75ae2049727616aba01e1740073bd4ab66e837b0
sahil n <sahil@mutterfly.in>
2018-12-24 17:58:14 +0530

    adds memoized userFetch function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
+import _ from 'lodash';
 
 export const fetchPosts = () => 
     async dispatch => {
@@ -6,8 +7,12 @@ export const fetchPosts = () =>
         dispatch({type: 'FETCH_POSTS', payload: response.data})
 }
 
-export const fetchUser = (id) => 
-    async dispatch => {
-        const response = await jsonPlaceholder.get(`/user/${id}`);
-        dispatch({type: 'FETCH_USER', payload: response.data})
-}
+// fetchUser is a function that returns a function that in turn calls _fetchUser
+// _fetchUser is memoized. ie. it returns the exact same data if called with repeated arguements.
+// memoization is useful to making API requests with the exact same ID as same data is returned.
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch)=>{
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+    dispatch({type: 'FETCH_USER', payload: response.data})
+})
